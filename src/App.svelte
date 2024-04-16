b7529d1659b3181bf5666ee8fc3dae29e2d4cf85
yodi.vandenhende <yodi.vandenhende@gmail.com>
2024-04-16 23:38:40 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Card from "./components/card-templates/card.svelte";
+</script>
+
+<main>
+  <div class="card">
+    <Card></Card>
+  </div>
+</main>
+
+<style>
+  main {
+    display: grid;
+    justify-content: center;
+    align-content: center;
+  }
+
+  .card {
+    --width: 500px;
+    width: var(--width);
+    height: calc(var(--width) * 1.39);
+  }
+</style>
---
+++ src/components/card-templates/card.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import AttackCard from "./attack-card.svelte";
+  import { CardTemplates, type CardData } from "./card-templates.type";
+  export let card = {
+    template: CardTemplates.attack,
+  };
+</script>
+
+<main>
+  {#if card.template === CardTemplates.attack}
+    <AttackCard></AttackCard>
+  {/if}
+</main>
+
+<style>
+  main {
+    height: 100%;
+  }
+</style>
