b63d0852b8619571df00c033b39fa31b24802007
Yodi <yodi.vandenhende@gmail.com>
2024-11-03 18:09:20 +0100

    debugging migration

--- src/migrations/2024-10-21T2239.js
+++ src/migrations/2024-10-21T2239.js
@@ -1,37 +1,31 @@
-import { fireStore } from "$lib/server/firebase.server";
-import { collection, getDocs } from "firebase/firestore";
+import { fireStore } from '$lib/server/firebase.server';
+import { collection, getDocs } from 'firebase/firestore';
 
 export async function migration() {
-    const [
-        decksQuery,
-        deckContentsQuery,
-        cardsQuery,
-    ] = await Promise.all([
-        await getDocs(collection(fireStore, 'Decks')),
-        await getDocs(collection(fireStore, 'DeckContents')),
-        await getDocs(collection(fireStore, 'Cards')),
-    ]);
+	const [decksQuery, deckContentsQuery, cardsQuery] = await Promise.all([
+		await getDocs(collection(fireStore, 'Decks')),
+		await getDocs(collection(fireStore, 'DeckContents')),
+		await getDocs(collection(fireStore, 'Cards'))
+	]);
 
-    const deckContents = deckContentsQuery.docs.map(doc => doc.data());
-    const cards = cardsQuery.docs.map(doc => doc.data());
-    const decks = decksQuery.docs.map(doc => doc.data()).map(deck => {
-        const deckCards = deckContents
-            .filter(content => deck.cards.include(content.id))
-            .map(content => {
-                const card = cards.find(card => content.cardId === card.id);
-                return {
-                    ...card,
-                    ...content,
-                }
-            })
+	const deckContents = deckContentsQuery.docs.map((doc) => doc.data());
+	const cards = cardsQuery.docs.map((doc) => doc.data());
+	let decks = decksQuery.docs.map((doc) => doc.data());
+	// decks = decks.map((deck) => {
+	// 	const deckCards = deckContents
+	// 		.filter((content) => deck.cards.include(content.id))
+	// 		.map((content) => {
+	// 			const card = cards.find((card) => content.cardId === card.id);
+	// 			return {
+	// 				...card,
+	// 				...content
+	// 			};
+	// 		});
+	// 	return {
+	// 		...deck,
+	// 		faceCard: deckCards[0]
+	// 	};
+	// });
 
-        return  {
-            ...deck,
-            faceCard: deckCards[0],
-        }
-
-    });
-
-
-    console.log(decks)
+	console.log(decks);
 }
--- src/routes/decks/+page.server.ts
+++ src/routes/decks/+page.server.ts
@@ -1,7 +1,7 @@
-import { getDecks } from "$lib/server/deck.server"
-import { migration } from "../../migrations/2024-10-21T2239"
+import { getDecks } from '$lib/server/deck.server';
+import { migration } from '../../migrations/2024-10-21T2239';
 
 export async function load() {
-  migration();
-  return { decks: await getDecks() }
+	migration();
+	return { decks: await getDecks() };
 }
