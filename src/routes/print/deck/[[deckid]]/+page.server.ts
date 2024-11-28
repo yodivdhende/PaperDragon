5371e1a0c2886103cdafd06662aa546895dca60c
Yodi <yodi.vandenhende@gmail.com>
2024-11-28 23:50:42 +0100

    fixing print download

--- src/routes/print/deck/[[deckid]]/+page.server.ts
+++ src/routes/print/deck/[[deckid]]/+page.server.ts
@@ -1,8 +1,28 @@
 import { getDeck } from '$lib/server/deck.server';
 
 export async function load({ params }: { params: { deckid: string } }) {
-  const { deckid } = params;
-  if (deckid == null) return;
-  const deck = await getDeck(deckid);
-  return { deck }
+	const { deckid } = params;
+	if (deckid == null) return;
+	const deck = await getDeck(deckid);
+	if (deck == null) return undefined;
+	const allCards = deck.cards.flatMap((card) => duplicateCards(card));
+	return {
+		deck: {
+			...deck,
+			cards: allCards
+		}
+	};
+}
+
+function duplicateCards(card: { id: string; amount: string }) {
+	const cards = [];
+	const amount = Number(card.amount);
+	if (isNaN(amount)) return [];
+	for (let index = 1; index <= amount; index++) {
+		cards.push({
+			...card,
+			id: `${card.id}-${amount}`
+		});
+	}
+	return cards;
 }
--- src/routes/print/deck/[[deckid]]/+page.svelte
+++ src/routes/print/deck/[[deckid]]/+page.svelte
@@ -1,21 +1,37 @@
 <script lang="ts">
 	import Card from '$lib/components/cards/card.svelte';
+	import CardSideSelector from '$lib/components/selectors/card-side-selector.svelte';
+	import SettingsLayout from '$lib/components/settings-layout.svelte';
+	import { selectedCardSideStore } from '$lib/services/card-selector.service';
+	import * as htmlToImage from 'html-to-image';
+	import download from 'downloadjs';
 
 	export let data;
 	const { deck } = data;
 
-	let mainWidth: number;
-	$: cardScale = mainWidth / 4 / 500;
-	$: console.log(mainWidth);
+	let mainElement: HTMLElement;
+	$: cardScale = mainElement?.clientWidth / 4 / 500;
+
+	async function exportPrintImg() {
+		if (deck == null) return console.error('no deck');
+		const url = await htmlToImage.toPng(mainElement);
+		download(url, `${deck.name}-${$selectedCardSideStore}`);
+	}
 </script>
 
-<div bind:clientWidth={mainWidth}>
-	{#if deck}
-		{#each deck.cards as card}
-			<Card {card} canEdit={false} scale={cardScale} />
-		{/each}
-	{/if}
-</div>
+<SettingsLayout>
+	<div bind:this={mainElement} slot="section">
+		{#if deck}
+			{#each deck.cards as card}
+				<Card {card} canEdit={false} scale={cardScale} />
+			{/each}
+		{/if}
+	</div>
+	<div slot="aside">
+		<CardSideSelector />
+		<button on:click={exportPrintImg}>download</button>
+	</div>
+</SettingsLayout>
 
 <style>
 	div {
